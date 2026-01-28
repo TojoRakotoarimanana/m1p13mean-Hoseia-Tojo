a465ed205c98046df5ef0ff47fab11e6040949c1
Rakotoarimanana <tianh01@protonmail.com>
2026-01-28 22:27:34 +0300

    Correction login et register

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -2,11 +2,29 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [CommonModule, FormsModule, RouterLink],
+    imports: [
+        CommonModule, 
+        FormsModule, 
+        RouterLink,
+        CardModule,
+        InputTextModule,
+        PasswordModule,
+        ButtonModule,
+        FloatLabelModule,
+        IconFieldModule,
+        InputIconModule
+    ],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
