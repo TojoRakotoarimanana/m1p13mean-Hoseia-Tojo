16072e1de036602f34df4fc6079032897e99eb25
Rakotoarimanana <tianh01@protonmail.com>
2026-03-02 23:27:01 +0300

    environnment prod

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://m1p13mean-hoseia-tojo.onrender.com'
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://m1p13mean-hoseia-tojo.onrender.com'
+  // apiUrl: 'http://localhost:3000'
 };
